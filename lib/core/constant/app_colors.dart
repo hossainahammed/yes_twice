607fc00abcea031cd29730f1f171a01831c8673c
hossainahammed <hossainahammed627@gmail.com>
2026-03-24 16:59:03 +0600

    Set app colors

--- lib/core/constant/app_colors.dart
+++ lib/core/constant/app_colors.dart
@@ -3,13 +3,18 @@ import 'package:flutter/material.dart';
 class AppColors {
   AppColors._();
 
-  static const Color primaryColor = Color(0xFF2D85F2);
-  static const Color textColor = Color(0xFF0F172B);
+  static const Color primaryColor = Color(0xFF4E070C);
+  static const Color secondaryColor = Color(0xFF8A5A42);
   static const Color subTextColor = Color(0xFF90A1B9);
+  static const Color chipColor = Color(0xFFFFEEBE2);
+  static const Color backGroundColor = Color(0xFFFFBF9F8);
+  static const Color GreyColor = Color(0xFFFB3B3B3);
+
+  static const Color textColor = Color(0xFF0F172B);
   static const Color boxTextColor = Color(0xFF5D5D5D);
 
   static const Color whiteColor = Color(0xFFFFFFFF);
-  static const Color buttonColor = Color(0xFF063D8F);
-  static const Color stockColor = Color(0xFFE0E8FC);
+  static const Color buttonColor = Color(0xFFF4E070C);
+  static const Color ratingColor = Color(0xFFFFC8838);
   static const Color redColor = Color(0xFFD9070B);
 }
