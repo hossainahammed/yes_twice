3d3c28be21db1009a4d4a14a164d0549a785d200
hossainahammed <hossainahammed627@gmail.com>
2026-03-25 09:10:59 +0600

    Fix appColors error

--- lib/core/constant/app_colors.dart
+++ lib/core/constant/app_colors.dart
@@ -6,15 +6,15 @@ class AppColors {
   static const Color primaryColor = Color(0xFF4E070C);
   static const Color secondaryColor = Color(0xFF8A5A42);
   static const Color subTextColor = Color(0xFF90A1B9);
-  static const Color chipColor = Color(0xFFFFEEBE2);
-  static const Color backGroundColor = Color(0xFFFFBF9F8);
-  static const Color GreyColor = Color(0xFFFB3B3B3);
+  static const Color chipColor = Color(0xFFFEEBE2);
+  static const Color backGroundColor = Color(0xFFFBF9F8);
+  static const Color greyColor = Color(0xFFB3B3B3);
 
   static const Color textColor = Color(0xFF0F172B);
   static const Color boxTextColor = Color(0xFF5D5D5D);
 
   static const Color whiteColor = Color(0xFFFFFFFF);
-  static const Color buttonColor = Color(0xFFF4E070C);
-  static const Color ratingColor = Color(0xFFFFC8838);
+  static const Color buttonColor = Color(0xFF4E070C);
+  static const Color ratingColor = Color(0xFFFC8838);
   static const Color redColor = Color(0xFFD9070B);
 }
